03940befaffb8d8d3b4fe6fa812b9424a6d2d8af
jcolmedo <119762688+jcolmedo@users.noreply.github.com>
2026-04-14 21:05:25 -0300

    Update astro.config.mjs
    configuramos astro para que levante la web

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://jcolmedo.github.io',
+  base: '/Portfolio',
   integrations: [react()],
   output: 'static',
   build: {
